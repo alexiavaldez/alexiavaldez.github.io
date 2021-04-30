9ec2df1c56e56186aea77a89d0aca76c409d39d2
alexiavaldez <77801374+alexiavaldez@users.noreply.github.com>
2021-04-30 13:48:28 -0600

    finished ex ch21

---
+++ projects/ch21/ch21-1.js
@@ -0,0 +1,11 @@
+// 1.
+var friends = ["Meghan", "Seth", "Perrin", "Marcus"];
+alert( Perrin );
+var name = "Alexia Valdez";
+if( name === Jennifer ) { alert("That's my name too!");}
+var myVariable = 8;
+if(myVariable > 5) {
+    alert ("upper")
+} else{
+    alert ("lower");
+}
